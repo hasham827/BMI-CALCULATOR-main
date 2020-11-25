0b9ef24ac477e091975d49c147d3829625b36a93
Hasham Ahmad <69302740+hasham827@users.noreply.github.com>
2020-11-25 20:32:31 +0500

    Update my_localizations.dart
    Bio about BMI starting screen.
    BMI Interpretation show after results

--- lib/my_localizations.dart
+++ lib/my_localizations.dart
@@ -1,18 +1,6 @@
 import 'dart:async';
-
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
-// This file was generated in two steps, using the Dart intl tools. With the
-// app's root directory (the one that contains pubspec.yaml) as the current
-// directory:
-//
-// flutter pub get
-// flutter pub pub run intl_translation:extract_to_arb --output-dir=lib/l10n lib/my_localizations.dart
-// flutter pub pub run intl_translation:generate_from_arb --output-dir=lib/l10n --no-use-deferred-loading lib/my_localizations.dart lib/l10n/intl_en.arb lib/l10n/intl_tr.arb
-//
-// The second command generates intl_messages.arb and the third generates
-// messages_all.dart. There's more about this process in
-// https://pub.dev/packages/intl.
 import 'package:bmicalculator/l10n/messages_all.dart';
 
 class MyLocalizations {
